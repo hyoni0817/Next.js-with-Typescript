cdc8caa9106b988470130bd7745f4c0d04397347
hyoni0817 <dudu1104@naver.com>
2021-12-05 13:26:10 +0900

    Fix: eslint 에러 해결을 위해 rules 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,17 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: {},
+  rules: {
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'arrow-body-style': ['error', 'always'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
+  },
   settings: {
     react: {
       version: 'detect',
